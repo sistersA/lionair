224fc2170c54d431d7a192022b0a0a89b34d89c7
dpay <deviap485@gmail.com>
2020-03-12 08:52:05 +0700

    file

--- RPC/c_rpc.py
+++ RPC/c_rpc.py
@@ -14,8 +14,25 @@ print()
 
 print("asal : ",s.asal(no))
 print("Tujuan : ",s.tujuan(no))
-print("boarding : ",s.inboard(no))
-print("Transit : ",s.insit(no))
+b=s.inboard(no)
+ins=s.insit(no)
+print("boarding : ",b)
+print("Transit : ",ins)
 
+
+
+
+# format board
+board = "No Penerbangan : {}\nBoarding: {}".format(no,b)
+transit = "No Penerbangan : {}\nBoarding: {}".format(no,ins)
+# buka file untuk ditulis
+file_board = open("boarding.txt", "w")
+file_transit = open("trans.txt", "w")
+# tulis board ke file
+file_board.write(board)
+file_transit.write(transit)
+# tutup file
+file_board.close()
+file_transit.close()
 
 #s.close()
